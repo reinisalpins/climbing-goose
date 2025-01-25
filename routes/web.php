41397f7040dd8465b26e5b71d3cef9abdf753cda
reinis <reinisalpins@gmail.com>
2025-01-26 00:56:39 +0200

    formatting

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('auth.login');
     Route::post('/login', [AuthController::class, 'login'])->name('auth.login.store');
-
     Route::get('/register', [AuthController::class, 'showRegister'])->name('auth.register');
     Route::post('/register', [AuthController::class, 'register'])->name('auth.register.store');
 });
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-
     Route::get('/profile/posts', [PostsController::class, 'showUserPosts'])->name('profile.posts.index');
     Route::get('/profile/posts/create', [PostsController::class, 'showCreatePostForm'])->name('profile.posts.create');
-
     Route::post('/posts', [PostsController::class, 'createPost'])->name('posts.store');
     Route::post('/posts/{post_id}/comments', [CommentsController::class, 'createComment'])->name('posts.comments.store');
 
