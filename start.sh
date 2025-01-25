2f12bcb6a24df1fcc183a5486c8dc2baa3fdccc7
reinis <reinisalpins@gmail.com>
2025-01-26 00:08:31 +0200

    add start script

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php84-composer:latest \
+    composer install --ignore-platform-reqs
+
+cp .env.example .env
+
+./vendor/bin/sail up -d
+
+sleep 10
+
+./vendor/bin/sail artisan key:generate
+./vendor/bin/sail artisan migrate
+./vendor/bin/sail artisan db:seed
+
+./vendor/bin/sail npm install
+./vendor/bin/sail npm run build
+
+echo "Setup completed successfully!"
+echo "Your application is running at: http://localhost"
